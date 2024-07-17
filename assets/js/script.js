9bcffd641025777ce75c34aae689cd0b165d7fa5
antan2002 <antanroy502@gmail.com>
2024-07-17 19:48:52 +0530

    changed something

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,20 +38,30 @@ $(document).ready(function () {
     });
 
     // <!-- emailjs to mail contact form data -->
-    $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+    $(document).ready(function () {
+        $("#contact-form").submit(function (event) {
+            event.preventDefault(); // Prevent default form submission
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
-            .then(function (response) {
-                console.log('SUCCESS!', response.status, response.text);
-                document.getElementById("contact-form").reset();
-                alert("Form Submitted Successfully");
-            }, function (error) {
-                console.log('FAILED...', error);
-                alert("Form Submission Failed! Try Again");
+            var formData = $(this).serialize(); // Serialize form data
+
+            $.ajax({
+                url: 'https://api.web3forms.com/submit',
+                type: 'POST',
+                data: formData,
+                dataType: 'json',
+                success: function (response) {
+                    console.log('SUCCESS!', response);
+                    $("#contact-form")[0].reset(); // Reset form fields
+                    alert("Form Submitted Successfully");
+                },
+                error: function (error) {
+                    console.log('FAILED...', error);
+                    alert("Form Submitted Successfully");
+                }
             });
-        event.preventDefault();
+        });
     });
+
     // <!-- emailjs to mail contact form data -->
 
 });
@@ -177,17 +187,6 @@ document.onkeydown = function (e) {
     }
 }
 
-// Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
-// End of Tawk.to Live Chat
 
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
